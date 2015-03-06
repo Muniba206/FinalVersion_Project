824b34b5eea7a190498139ee547dbe225fef9356
Torni <Carina_17@live.nl>
2015-03-06 12:51:51 +0100

    Client
    Updated methods

--- src/model/Client.java
+++ src/model/Client.java
@@ -51,6 +51,102 @@ public class Client  {
 	}
 	
 	/*
+	 * To start the dialog
+	 */
+	
+	public boolean start() {
+		// Try to connect to the server
+		try {
+			socket = new Socket(server, port);
+		} 
+		// If it failed not much can be done
+		catch(Exception ec) {
+			display("Error connectiong to server:" + ec);
+			return false;
+		}
+		
+		String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+		display(msg);
+	
+		
+		/* Creating both data streams */
+		try
+		{
+			sInput  = new ObjectInputStream(socket.getInputStream());
+			sOutput = new ObjectOutputStream(socket.getOutputStream());
+		}
+		catch (IOException eIO) {
+			display("Exception creating new Input/output Streams: " + eIO);
+			return false;
+		}
+
+		// creates the Thread to listen from the server 
+		new ListenFromServer().start();
+		// Send our username to the server, this is the only message that we
+		// will send as a String. All other messages will be ChatMessage objects
+		try
+		{
+			sOutput.writeObject(username);
+		}
+		catch (IOException eIO) {
+			display("Exception doing login : " + eIO);
+			disconnect();
+			return false;
+		}
+		// success, we inform the caller that it worked
+		return true;
+	}
+
+	/*
+	 * To send a message to the console or the GUI
+	 */
+	
+	private void display(String msg) {
+		if(cg == null)
+			System.out.println(msg);      // println in console mode
+		else
+			cg.append(msg + "\n");		// append to the ClientGUI JTextArea (or something)
+	}
+	
+	/*
+	 * To send a message to the server
+	 */
+	
+	public void sendMessage(ChatMessage msg) {
+		try {
+			sOutput.writeObject(msg);
+		}
+		catch(IOException e) {
+			display("Exception writing to server: " + e);
+		}
+	}
+
+	/*
+	 * When something goes wrong
+	 * Close the Input/Output streams and disconnect, not much to do in the catch clause
+	 */
+	
+	private void disconnect() {
+		try { 
+			if(sInput != null) sInput.close();
+		}
+		catch(Exception e) {} // not much can be done
+		try {
+			if(sOutput != null) sOutput.close();
+		}
+		catch(Exception e) {} // not much can be done
+        try{
+			if(socket != null) socket.close();
+		}
+		catch(Exception e) {} // not much can be done
+		
+		// Inform the GUI
+		if(cg != null)
+			cg.connectionFailed();
+			
+	}
+	
+	/*
 	 *  To run as a console application just open a console window and if the port number 
 	 *  is not specified, 1500 is used.
 	 *  
@@ -125,5 +221,38 @@ public class Client  {
 		// Done, disconnect
 		client.disconnect();	
 	}	
+	
+	/*
+	 * A class that waits for the message from the server and append them to the JTextArea
+	 * if we have a GUI or simply System.out.println() it in console mode
+	 */
+	
+	class ListenFromServer extends Thread {
+
+		public void run() {
+			while(true) {
+				try {
+					String msg = (String) sInput.readObject();
+					// If console mode, print the message and add back the prompt
+					if(cg == null) {
+						System.out.println(msg);
+						System.out.print("> ");
+					}
+					else {
+						cg.append(msg);
+					}
+				}
+				catch(IOException e) {
+					display("Server has close the connection: " + e);
+					if(cg != null) 
+						cg.connectionFailed();
+					break;
+				}
+				// Can't happen with a String object but still need to catch it
+				catch(ClassNotFoundException e2) {
+				}
+			}
+		}
+	}
 	
 }
