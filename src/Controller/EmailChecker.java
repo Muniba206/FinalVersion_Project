e2371d6cf19c26e8789f237c21c3d8a894b568bd
lina1206 <lina0815@stud.kea.dk>
2015-03-08 08:25:06 +0100

    Email controller
    Checking if the user Email is valid

---
+++ src/Controller/EmailChecker.java
@@ -0,0 +1,17 @@
+
+public class EmailChecker {
+	
+	
+	public static boolean isValidEmailAddress(String email) {
+		
+		   boolean result = true;
+		   try {
+		      InternetAddress emailAddr = new InternetAddress(email);
+		      emailAddr.validate();
+		   } catch (AddressException ex) {
+		      result = false;
+		   }
+		   return result;
+		}
+
+}
